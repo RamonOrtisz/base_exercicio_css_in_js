78a5660f43c3fd9b5879e81a951da7d7b39c77c0
RamonOrtisz <www.gamesetutoriaiss@gmail.com>
2024-01-24 17:28:33 -0300

    começando o atividade

---
+++ src/containers/ListaVagas/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const vagas = styled.div `
+  .vagas {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 16px;
+    row-gap: 16px;
+    margin-top: 32px;
+  }
+
+  @media (max-width: 768px) {
+    .vagas {
+      grid-template-columns: 1fr;
+    }
+  }
+`
